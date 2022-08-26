34d700efbba6fd1147ef6d943cbe30d1712b3cd2
mujunpeng <13854260918@163.com>
2022-08-26 17:59:57 +0800

    [add]:传入泛型方法的参数是数组的测试

--- src/main/java/com/mjp/basic/PairTest1.java
+++ src/main/java/com/mjp/basic/PairTest1.java
@@ -4,15 +4,26 @@ public class PairTest1 {
 
     public static void main(String[] args) {
 
-        String[] words = {"a","b","c","cd"};
+        String[] words = {"a","b","c","dc"};
 
         Pair<String> minmax = ArrayAlg.minmax(words);
         System.out.println("min is " + minmax.getFirst());
         System.out.println("max is " + minmax.getSecond());
 
-        int[] words2 = {1,2,3,4,5};
+        String[] words2 = {"a","b","c","d"};
         int middle = ArrayAlg.getMiddle(1,2,3,4,5);
         System.out.println("middle is " + middle);
+
+        Pair<String> min = ArrayAlg.min(words2);
+        System.out.println("min is " + min.getFirst() + "max is " + min.getSecond());
+
+        String s = ArrayAlg.minbyArray(words2);
+        System.out.println("last min is " + s);
+
+        Integer[] inputInteger = {Integer.parseInt("1111"),Integer.parseInt("2"),Integer.parseInt("3")};
+
+        Integer mininteger = ArrayAlg.minbyArray(inputInteger);
+        System.out.println("min integer is " + mininteger);
     }
 
 }
@@ -47,7 +58,45 @@ class ArrayAlg{
         return a[a.length/2];
     }
 
+    /**
+     * 求最小值 ，并且增加类型的限定
+     * @param a
+     * @param <T>
+     * @return
+     */
+    public static <T extends Comparable> Pair<T> min(T[] a){
 
+        if(a == null || a.length == 0){
+            return null;
+        }
+
+        T smallest = a[0];
+        for(int i = 0;i < a.length;i++){
+            if(smallest.compareTo(a[i]) > 0) smallest = a[i];
+        }
+
+        return new Pair<>(smallest,smallest);
+    }
+
+    /**
+     * 求最小值 ，并且增加类型的限定
+     * @param a
+     * @param <T>
+     * @return
+     */
+    public static <T extends Comparable> T minbyArray(T[] a){
+
+        if(a == null || a.length == 0){
+            return null;
+        }
+
+        T smallest = a[0];
+        for(int i = 0;i < a.length;i++){
+            if(smallest.compareTo(a[i]) > 0) smallest = a[i];
+        }
+
+        return smallest;
+    }
 
 
 }
